12633fb1bb4e0e1ca77a77400ac8073a38b86a24
Juan Pablo Valenzuela <92190741+iTzTorero@users.noreply.github.com>
2022-02-23 12:48:46 -0700

    Practica5_update

--- TheCheezery/app/src/main/java/valenzuela/juan/thecheezery/MenuActivity.kt
+++ TheCheezery/app/src/main/java/valenzuela/juan/thecheezery/MenuActivity.kt
@@ -11,8 +11,33 @@ class MenuActivity : AppCompatActivity() {
         setContentView(R.layout.activity_menu)
 
         var btnCold: Button = findViewById(R.id.button_cold_drinks) as Button
+        var btnHot: Button = findViewById(R.id.button_cold_drinks) as Button
+        var btnSweets: Button = findViewById(R.id.button_cold_drinks) as Button
+        var btnSalties: Button = findViewById(R.id.button_cold_drinks) as Button
+
+
+
         btnCold.setOnClickListener {
             var intent:Intent = Intent(this,ProductosActivity::class.java)
+            intent.putExtra("menuType","coldDrinks")
+            startActivity(intent)
+        }
+
+        btnHot.setOnClickListener {
+            var intent:Intent = Intent(this,ProductosActivity::class.java)
+            intent.putExtra("menuType","hotDrinks")
+            startActivity(intent)
+        }
+
+        btnSweets.setOnClickListener {
+            var intent:Intent = Intent(this,ProductosActivity::class.java)
+            intent.putExtra("menuType","sweets")
+            startActivity(intent)
+        }
+
+        btnSalties.setOnClickListener {
+            var intent:Intent = Intent(this,ProductosActivity::class.java)
+            intent.putExtra("menuType","salties")
             startActivity(intent)
         }
     }
--- TheCheezery/app/src/main/java/valenzuela/juan/thecheezery/ProductosActivity.kt
+++ TheCheezery/app/src/main/java/valenzuela/juan/thecheezery/ProductosActivity.kt
@@ -13,24 +13,106 @@ import android.widget.TextView
 import org.w3c.dom.Text
 
 class ProductosActivity : AppCompatActivity() {
-    var coldDrinks=ArrayList<Product>()
+    var menu:ArrayList<Product> = ArrayList<Product>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_productos)
-        agregarProdutos()
+        var menuOption:String? = intent.getStringExtra("menuType")
+        agregarProdutos(menuOption)
         var listView:ListView =findViewById(R.id.litview) as ListView
-        var adaptador: AdaptadorProductos=AdaptadorProductos(this,coldDrinks)
+        var adaptador: AdaptadorProductos=AdaptadorProductos(this,menu)
         listView.adapter=adaptador
 
     }
 
-    fun agregarProdutos(){
-        coldDrinks.add(Product("Caramel Frap", R.drawable.caramelfrap, "Caramel syrup meets coffee, milk and ice and whipped cream and buttery caramel sauce layer the love on top.", 5.0))
-        coldDrinks.add(Product("Chocolate Frap", R.drawable.chocolatefrap, "Rich mocha-flavored sauce meets up with chocolaty chips, milk and ice for a blender bash.", 6.0))
-        coldDrinks.add(Product("Cold Brew", R.drawable.coldbrew, "Created by steeping medium-to-coarse ground coffee in room temperature water for 12 hours or longer.", 3.0))
-        coldDrinks.add(Product("Matcha Latte", R.drawable.matcha, "Leafy taste of matcha green tea powder with creamy milk and a little sugar for a flavor balance that will leave you feeling ready and raring to go.", 4.0))
-        coldDrinks.add(Product("Oreo Milkshake", R.drawable.oreomilkshake, "Chocolate ice cream, and oreo cookies. Topped with whipped cream with cocoa and chocolate syrup.", 7.0))
-        coldDrinks.add(Product("Peanut Milkshake", R.drawable.peanutmilkshake, "Vanilla ice cream, mixed with peanut butter and chocolate.", 7.0))
+    fun agregarProdutos(option:String?){
+        var imageView: ImageView= findViewById<ImageView>(R.id.imgMenu)
+
+        when(option){
+            "coldDrinks"-> {
+                imageView.setImageResource(R.drawable.colddrinks)
+                menu.add(
+                    Product(
+                        "Caramel Frap",
+                        R.drawable.caramelfrap,
+                        "Caramel syrup meets coffee, milk and ice and whipped cream and buttery caramel sauce layer the love on top.",
+                        5.0
+                    )
+                )
+                menu.add(
+                    Product(
+                        "Chocolate Frap",
+                        R.drawable.chocolatefrap,
+                        "Rich mocha-flavored sauce meets up with chocolaty chips, milk and ice for a blender bash.",
+                        6.0
+                    )
+                )
+                menu.add(
+                    Product(
+                        "Cold Brew",
+                        R.drawable.coldbrew,
+                        "Created by steeping medium-to-coarse ground coffee in room temperature water for 12 hours or longer.",
+                        3.0
+                    )
+                )
+                menu.add(
+                    Product(
+                        "Matcha Latte",
+                        R.drawable.matcha,
+                        "Leafy taste of matcha green tea powder with creamy milk and a little sugar for a flavor balance that will leave you feeling ready and raring to go.",
+                        4.0
+                    )
+                )
+                menu.add(
+                    Product(
+                        "Oreo Milkshake",
+                        R.drawable.oreomilkshake,
+                        "Chocolate ice cream, and oreo cookies. Topped with whipped cream with cocoa and chocolate syrup.",
+                        7.0
+                    )
+                )
+                menu.add(
+                    Product(
+                        "Peanut Milkshake",
+                        R.drawable.peanutmilkshake,
+                        "Vanilla ice cream, mixed with peanut butter and chocolate.",
+                        7.0
+                    )
+                )
+            }
+            "hotDrinks"-> {
+            imageView.setImageResource(R.drawable.hotdrinks)
+                menu.add(Product("Latte", R.drawable.latte, "Coffee drink made with espresso and steamed milk", 6.0))
+                menu.add(Product("Hot chocolate", R.drawable.hotchocolate, "Heated drink consisting of shaved chocolate, topped with marshmallows.", 5.0))
+                menu.add(Product("Espresso", R.drawable.espresso, "Full-flavored, concentrated form of coffee.", 4.0))
+                menu.add(Product("Chai Latte", R.drawable.chailatte, "Spiced tea concentrate with milk", 6.0))
+                menu.add(Product("Capuccino", R.drawable.capuccino, "A cappuccino is an espresso-based coffee drink, prepared with steamed foam.", 7.0))
+                menu.add(Product("American coffee", R.drawable.americano, "Espresso with hot water", 2.0))
+
+            }
+
+            "salties"->{
+                imageView.setImageResource(R.drawable.salties)
+                menu.add(Product("Chicken crepes", R.drawable.chickencrepes, "Fine crepes stuffed with Alfredo chicken, spinach and mushrooms.", 6.0))
+                menu.add(Product("Club Sandwich", R.drawable.clubsandwich, "A delicious sandwich served with french fries.", 5.0))
+                menu.add(Product("Panini", R.drawable.hampanini, "Sandwich made with Italian bread  served warmed by grilling.", 4.0))
+                menu.add(Product("Philly cheese steak", R.drawable. phillycheesesteak, "Smothered in grilled onions, green peppers, mushrooms, and Provolone.", 6.0))
+                menu.add(Product("Nachos", R.drawable. nachos, "Tortilla chips layered with beef and   melted cheddar cheese. Served with fried beans, guacamole, pico de gallo, and sour topping.",  7.0))
+
+            }
+            "sweets"->{
+                imageView.setImageResource(R.drawable.sweets)
+                menu.add(Product("Blueberry cake", R.drawable.blueberrycake, "Vanilla cake flavor, topped with cheese topping and blueberries.", 6.0))
+                menu.add(Product("Chocolate cupcake", R.drawable.chocolatecupcake, "Chocolate cupcakes topped with butter cream and cherries", 3.0))
+                menu.add(Product("Lemon tartalette", R.drawable.lemontartalette, "Pastry shell with a lemon flavored filling", 4.0))
+                menu.add(Product("Red Velvet cake", R.drawable.redvelvetcake, "Soft, moist, buttery cake topped with an easy cream cheese frosting.", 6.0))
+                menu.add(Product("Cherry cheesecake", R.drawable. strawberrycheesecake, "This cherry topped cheesecake is positively creamy and delicious and will be your new favorite dessert.", 7.0))
+                menu.add(Product("Tiramisu", R.drawable.tiramisu, "Coffee-flavored Italian dessert", 6.0))
+
+            }
+        }
+
     }
 
     private class AdaptadorProductos: BaseAdapter {
